d43c86787c090b76c04492f21e192ba569586165
ruslan <ruslan.v@pyrobyte.ru>
2023-06-09 12:38:07 +0700

    patterns behavioral memento

---
+++ App/Patterns/Behavioral/Memento/EditorMemento.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Patterns\Behavioral\Memento;
+
+class EditorMemento
+{
+    protected $content;
+
+    public function __construct(string $content)
+    {
+        $this->content = $content;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
